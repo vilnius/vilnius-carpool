f460ee01af6ee6c441b174c101e69b08ee7df587
Marijus <spastai@gmail.com>
2016-04-19 18:50:09 +0300

    Some pain now - it took some while to change polyline color

--- src/packages/carpool-view-mui/components/map/GoogleMap.jsx
+++ src/packages/carpool-view-mui/components/map/GoogleMap.jsx
@@ -1,6 +1,6 @@
 import PageRoot from '../layout/PageRoot'
 
-import {GoogleMapLoader, GoogleMap, Marker} from "react-google-maps";
+import {GoogleMapLoader, GoogleMap, Marker, Polyline} from "react-google-maps";
 import { createContainer } from 'meteor/react-meteor-data';
 import { ReactiveVar } from 'meteor/reactive-var';
 import { default as _ } from "lodash";
@@ -10,14 +10,23 @@ export default class ReactMap extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-        markers: [{
-          position: {
-            lat: 25.0112183,
-            lng: 121.52067570000001,
-          },
-          key: `Taiwan`,
-          defaultAnimation: 2,
-       }],
+      markers: [{
+        position: {
+          lat: 54.6877209,
+          lng: 25.305228100000022,
+        },
+        key: `From`,
+        defaultAnimation: 2,
+        icon: 'http://maps.google.com/mapfiles/ms/icons/red-dot.png'
+      },{
+       position: {
+         lat: 54.6779097,
+         lng: 25.26246500000002,
+       },
+       key: `To`,
+       defaultAnimation: 2,
+       icon: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png'
+     }],
     }
     this.handleWindowResize = _.throttle(this.handleWindowResize, 500);
   }
@@ -36,6 +45,12 @@ export default class ReactMap extends React.Component {
         </section>
       );
     } else
+      trip = {
+        path: google.maps.geometry.encoding.decodePath("iexlI}lmyCn@q@dC{BfBqBbAqA\\o@z@yAvAuC~@_CdAiDr@kB^mAjBiE^q@^e@n@m@vAoAxAs@lAGrEz@hB`Av@`AlAxANPd@l@xAlBbCvCbDbFnAjC^v@n@fBj@lB`@dBvB`L~ClVxCrNh@dB\\dBr@xDb@rCNbCLjEJpCPlCR`BlAzHdAuAXMzAdD`CnE`@bAn@~Bp@hBjAjC|@|At@`Av@p@Xf@^fAx@vC~@lE~A|GXhCPdBp@~JV|DpAlNTvCPzC^lEeBd@wBp@ULi@b@aDrD_C|BcBz@iFbA{Cz@sBx@{@P}@TsHdBwCp@cCr@oHnBoBf@x@xJNxANvCVlDk@Pl@bIMEoC{A[GG?"),
+        strokeOpacity: 0.5,
+        strokeColor: "#FF0000",
+      }
+
       return (
         <section style={{height: "100%"}}>
           <GoogleMapLoader
@@ -49,8 +64,20 @@ export default class ReactMap extends React.Component {
             }
             googleMapElement={
               <GoogleMap
-                defaultZoom={3}
-                defaultCenter={{lat: -25.363882, lng: 131.044922}}>
+                defaultZoom={12}
+                defaultCenter={{lat: 54.67704, lng: 25.25405}}>
+                {this.state.markers.map((marker, index) => {
+                  return (
+                    <Marker {...marker} />
+                  );
+                })}
+                <Polyline {...trip}
+                    options={{
+                      strokeColor: "#0080BD",
+                      strokeOpacity: 1,
+                      strokeWeight: 3
+                      }}
+                />
               </GoogleMap>
             }
           />
