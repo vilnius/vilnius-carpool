910906df866acc4972f250bc2fc8cbeb87cf1454
Marijus <spastai@gmail.com>
2016-04-20 08:49:21 +0300

    Changes on rideOffers to comply cucumber tests

--- src/packages/carpool-view-mui/components/screens/RideOffers.jsx
+++ src/packages/carpool-view-mui/components/screens/RideOffers.jsx
@@ -10,35 +10,11 @@ import Tab from 'material-ui/lib/tabs/tab';
 import FloatingActionButton from 'material-ui/lib/floating-action-button';
 import ContentAdd from 'material-ui/lib/svg-icons/content/add';
 import HamburgerMenuButton from './components/HamburgerMenuButton'
+import { createContainer } from 'meteor/react-meteor-data';
+
 
 import wrapMobileLayout from './NewMobileWrap'
 import config from './config'
-
-const offers = [{
-  image: 'http://lorempixel.com/200/200/people/0',
-  name: 'Ana',
-}, {
-  image: 'http://lorempixel.com/200/200/people/1',
-  name: 'Bob',
-}, {
-  image: 'http://lorempixel.com/200/200/people/2',
-  name: 'Caithlyn',
-}, {
-  image: 'http://lorempixel.com/200/200/people/3',
-  name: 'David',
-}, {
-  image: 'http://lorempixel.com/200/200/people/4',
-  name: 'Erik',
-}, {
-  image: 'http://lorempixel.com/200/200/people/5',
-  name: 'Frank',
-}, {
-  image: 'http://lorempixel.com/200/200/people/6',
-  name: 'George',
-},{
-  image: 'http://lorempixel.com/200/200/people/7',
-  name: 'Harry',
-}]
 
 const filledCircleStyle = {
   background: config.colors.main,
@@ -69,6 +45,8 @@ const emptyCircleStyle = {
 
 export default class RideOffers extends React.Component {
   render () {
+    const { progress, activeTrips, ownTrips} = this.props;
+
     return (
       <div style={{paddingBottom: 64}}>
         <TopBar
@@ -83,22 +61,22 @@ export default class RideOffers extends React.Component {
           rightIcon={'o'}
         />
         <List>
-          {offers.map((offer) => {
+          {activeTrips.map((trip) => {
             return (
               [<ListItem key={1}
                 onClick={() => muiControllerHelper.goToView('MuiRequestRide')}
                 rightAvatar={
                   <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', top: 4, height: '100%'}}>
-                    <Avatar src={offer.image} size={50} />
-                    <span style={{fontSize: 11, marginTop: 5, color: '#9B9B9B', fontWeight: 500}}>{offer.name}</span>
+                    <Avatar src='http://lorempixel.com/200/200/people/1' size={50} />
+                    <span style={{fontSize: 11, marginTop: 5, color: '#9B9B9B', fontWeight: 500}}>{trip.name}</span>
                   </div>
                 }
               >
                 <div style={{display: 'flex', flexDirection: 'column'}}>
-                  <div style={{marginBottom: 7, fontSize: 14}}>From gatves g. 12 8:30</div>
-                  <div style={{marginBottom: 10, fontSize: 14}}>To prospekto pr. 57 ~9:00</div>
+                  <div style={{marginBottom: 7, fontSize: 14}}>From: {trip.fromAddress}</div>
+                  <div style={{marginBottom: 10, fontSize: 14}}>To: {trip.toAddress}</div>
                   <div>{Math.random() > 0.6 ? (
-                    <span style={{fontSize: 12}}>Jun 18, 2016</span>
+                    <span style={{fontSize: 12}}>{moment(trip.time).format("lll")}</span>
                   ) : (
                     <div style={{display: 'flex', flexDirection: 'row'}}>
                       {['M', 'T', 'W', 'T', 'F', 'S'].map((day, i) => (
@@ -120,7 +98,8 @@ export default class RideOffers extends React.Component {
             right: 12,
             bottom: 75,
           }}
-          onClick={() => muiControllerHelper.goToView('NewRideOffer')}
+          className="addTrip-test"
+          onClick={() => {muiControllerHelper.goToView('MuiEditTrip')}}
         >
           <ContentAdd />
         </FloatingActionButton>
@@ -129,4 +108,21 @@ export default class RideOffers extends React.Component {
   }
 }
 
-RideOffersScreen = RideOffers
+RideOffers.propTypes = {
+  progress: React.PropTypes.object,
+  activeTrips: React.PropTypes.array,
+  ownTrips: React.PropTypes.array
+};
+
+
+RideOffersScreen = createContainer(() => {
+  const progress = new Progress();
+  const activeTrips = carpoolService.pullActiveTrips({}, progress.setProgress.bind(progress, 'activeTrips'));
+  const ownTrips = carpoolService.pullOwnTrips({}, progress.setProgress.bind(progress, 'ownTrips'));
+
+  return {
+    progress,
+    activeTrips,
+    ownTrips,
+  };
+}, RideOffers);
