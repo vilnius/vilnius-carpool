2a0547209cbdf695109ce668ee988a3799ffef5d
Marijus <spastai@gmail.com>
2016-05-11 15:32:50 +0300

    Added compoments

--- src/packages/carpool-view-flow/router.js
+++ src/packages/carpool-view-flow/router.js
@@ -2,32 +2,33 @@ export const name = 'carpool-view-flow';
 
 import React from 'react';
 import {mount} from 'react-mounter';
-import {MainLayout} from './layout'
+import {LandingLayout} from './layout'
 import BottomTabs from "./react/layout/BottomTabs"
 
-
-FlowRouter.route('/:ownTrips?', {
-    name: "RideOffers",
-    action: function(params, queryParams) {
-      console.log("Yeah! We are on root.");
-      mount(MainLayout, {
-        topMenu: <OffersMenu />,
-        topFilter: <AllYourFilter />,
-        content: <RideList name="Arunoda" />,
-        bottomMenu: <BottomTabs />,
-      });
-    }
-});
+import RideOffersScreen from './react/ride-offers/RideOffersScreen'
 
 FlowRouter.route('/requests/:ownTrips?', {
   name: "Requests",
   action: function(params, queryParams) {
-    console.log("Yeah! We are on root.");
-    mount(MainLayout, {
+    console.log("Yeah! We are on requests");
+    mount(LandingLayout, {
       topMenu: <RequestsMenu />,
       topFilter: <AllYourFilter />,
       content: <RideList name="Arunoda" />,
       bottomMenu: <BottomTabs />,
     });
   }
+});
+
+FlowRouter.route('/:ownTrips?', {
+    name: "RideOffers",
+    action: function(params, queryParams) {
+      console.log("Yeah! We are on root.");
+      mount(LandingLayout, {
+        topMenu: <OffersMenu />,
+        topFilter: <AllYourFilter />,
+        content: <RideOffersScreen/>,
+        bottomMenu: <BottomTabs />,
+      });
+    }
 });
